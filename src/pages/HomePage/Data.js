52a1a70adc5d28960376568ff2710a966a02464d
Marie Grace Bodur <mgracebodur@gmail.com>
2021-07-16 10:12:45 -0400

    Add content sections and images in Home page

--- src/pages/HomePage/Data.js
+++ src/pages/HomePage/Data.js
@@ -1,6 +1,9 @@
-import homepageImg from "../../assets/teddybear.svg";
+import teddybear from "../../assets/teddybear.svg";
+import toycar from "../../assets/toycar.svg";
+import videogame from "../../assets/videogame.svg";
+import assortedToys from "../../assets/assorted-toys.jpg";
 
-export const homeObjOne = {
+export const contentOne = {
   lightBg: false,
   primary: true,
   imgStart: "",
@@ -13,6 +16,57 @@ export const homeObjOne = {
   headline: "Exchange Toys for a Clutter-Free Home at Little to No Cost",
   topLine: "Trade Toys",
   start: "",
-  img: homepageImg,
+  img: teddybear,
   alt: "A girl with a teddy bear",
+};
+
+export const contentTwo = {
+  lightBg: true,
+  primary: false,
+  imgStart: "start",
+  lightTopLine: false,
+  lightText: false,
+  lightTextDesc: false,
+  buttonLabel: "Get Started",
+  description:
+    "Trade your gently used children’s toys with other local members. Simply list items you’re willing to exchange, then negotiate with the lister of an item that you want. Once you come to an arrangement that makes you both happy, you send out the toys.",
+  headline: "Exchange Toys for a Clutter-Free Home at Little to No Cost",
+  topLine: "Trade Toys",
+  start: "true",
+  img: assortedToys,
+  alt: "A boy playing with his car",
+};
+
+export const contentThree = {
+  lightBg: false,
+  primary: true,
+  imgStart: "",
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  buttonLabel: "Get Started",
+  description:
+    "Trade your gently used children’s toys with other local members. Simply list items you’re willing to exchange, then negotiate with the lister of an item that you want. Once you come to an arrangement that makes you both happy, you send out the toys.",
+  headline: "Exchange Toys for a Clutter-Free Home at Little to No Cost",
+  topLine: "Trade Toys",
+  start: "",
+  img: videogame,
+  alt: "A girl and a boy with a game console",
+};
+
+export const contentFour = {
+  lightBg: false,
+  primary: true,
+  imgStart: "start",
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  buttonLabel: "Get Started",
+  description:
+    "Trade your gently used children’s toys with other local members. Simply list items you’re willing to exchange, then negotiate with the lister of an item that you want. Once you come to an arrangement that makes you both happy, you send out the toys.",
+  headline: "Exchange Toys for a Clutter-Free Home at Little to No Cost",
+  topLine: "Trade Toys",
+  start: "true",
+  img: toycar,
+  alt: "A girl and a boy with a game console",
 };
--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ContentSection from "../../components/ContentSection/ContentSection";
-import { homeObjOne } from "./Data";
+import { contentOne, contentTwo, contentThree, contentFour } from "./Data";
 
 const Home = () => {
   return (
     <>
-      <ContentSection {...homeObjOne}/>
+      <ContentSection {...contentOne}/>
+      <ContentSection {...contentTwo}/>
+      <ContentSection {...contentThree}/>
+      <ContentSection {...contentFour}/>
     </>
   );
 };
